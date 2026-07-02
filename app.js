e8c96cbe9aa37cc0ebf4e9d84ba2511f17ef178f
oracle <oracle@lnx142.home.aarisha.com>
2026-07-02 11:28:37 -0400

    dude

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
+
 app.get('/', (req, res) => res.send('Hello from Jenkins CD Pipeline!'));
-app.listen(3000);
+
+// Only listen if the file is run directly (not required/imported by a test framework)
+if (require.main === module) {
+  app.listen(3000, () => console.log('Server running on port 3000'));
+}
+
 module.exports = app;
+
